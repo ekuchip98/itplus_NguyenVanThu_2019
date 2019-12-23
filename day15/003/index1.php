c9b511f2fabfd79266b7ea5cddd4d2b20b51d985
ekuchip01 <ekuchip01@gmail.com>
2019-12-23 18:44:54 +0700

    55

---
+++ day15/003/index1.php
@@ -0,0 +1,12 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 12/23/2019
+ * Time: 6:36 PM
+ */
+include_once "function.php";
+$bk = 10;
+$kq = tinhdientichhinhtron($bk);
+echo $kq;
